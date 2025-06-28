edd4fef03aefc90c917abfb34c033b95ddfe6518
Edison22O <godoyedison465@gmail.com>
2025-06-28 15:16:50 -0500

    redireccionamiento

--- apps/users/views.py
+++ apps/users/views.py
@@ -21,7 +21,6 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
-
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
--- config/settings.py
+++ config/settings.py
@@ -149,9 +149,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # URLs de autenticación
-LOGIN_URL = '/accounts/login/'           # Cambiado para allauth
+LOGIN_URL = '/users/login/'           # Cambiado para allauth
 LOGIN_REDIRECT_URL = '/dashboard/'       # Después del login exitoso
-LOGOUT_REDIRECT_URL = '/accounts/login/' # Después del logout
+LOGOUT_REDIRECT_URL = '/users/login/' # Después del logout
 
 # Configuración de allauth
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
